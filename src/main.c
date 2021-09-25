2a0f68ba584cbea5c0dfc5269a75f18d6e3b1763
jhonatan229 <jhonatan229@outlook.com>
2021-09-24 21:50:35 -0300

    implementando o funcionamento do ft_env

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: jestevam < jestevam@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 16:44:20 by mavinici          #+#    #+#             */
-/*   Updated: 2021/09/24 21:39:52 by jestevam         ###   ########.fr       */
+/*   Updated: 2021/09/24 21:50:01 by jestevam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-extern char **envi;
+extern char** environ;
 
 char	*get_command(void)
 {
@@ -26,9 +26,7 @@ char	*get_command(void)
 	command = readline(prompt);
 	//printf("%s\n", command);
 	if (!ft_strcmp(command, "exit"))
-	{
 		exit(0);
-	}
 	free(prompt);
 	add_history(command);
 	return (command);
@@ -42,8 +40,8 @@ int	check_command(char *command)
 		ft_pwd();
 	else if (!ft_strncmp(command, "cd", 2))
 		printf("CDCDHEHHE\n");
-	else if (!ft_strncmp(command, "env", 3))
-		ft_env(envi);
+	else if (!ft_strcmp(command, "env"))
+		ft_env(environ);
 	else
 		printf("%s: command not found\n", command);
 	return (0);
