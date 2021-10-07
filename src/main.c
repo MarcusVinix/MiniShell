231682f1a1237f54b57d0f3fd748e0cfdceeadc8
jhonatan229 <jhonatan229@outlook.com>
2021-10-06 22:34:06 -0300

    concertando leak do readline

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: jestevam < jestevam@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 16:44:20 by mavinici          #+#    #+#             */
-/*   Updated: 2021/10/06 22:04:18 by jestevam         ###   ########.fr       */
+/*   Updated: 2021/10/06 22:31:58 by jestevam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	val_return_process;
+int	status;
 
 void	get_command(t_shell  *shell)
 {
@@ -25,6 +25,8 @@ void	get_command(t_shell  *shell)
 	tmp = ft_strjoin("\033[33m", cwd);
 	prompt = ft_strjoin(tmp, "$\033[0m ");
 	free(tmp);
+	if (shell->command)
+		free(shell->command);
 	shell->command = readline(prompt);
 	shell->split_cmd = ft_split(shell->command, ' ');
 	//printf("%s\n", command);
@@ -35,7 +37,7 @@ void	get_command(t_shell  *shell)
 			ret = ft_atoi(shell->split_cmd[1]);
 		free(prompt);
 		free_all(shell);
-		exit(val_return_process);
+		exit(status);
 	}
 	free(prompt);
 	add_history(shell->command);
@@ -52,12 +54,12 @@ int	check_command(t_shell *shell)
 	else if (ft_strcmp(shell->split_cmd[0], "env") == 0)
 		ft_env(shell);
 	else if (ft_strcmp(shell->split_cmd[0], "export") == 0)
-		val_return_process = ft_export(shell);
+		status = ft_export(shell);
 	else if (ft_strcmp(shell->split_cmd[0], "unset") == 0)
 		ft_unset(shell, &shell->lst_env);
 	else
 		if (execve(shell->split_cmd[0], shell->split_cmd, (char *const *)shell->lst_env) == -1)
-			val_return_process = not_found(shell->split_cmd[0]);
+			status = not_found(shell->split_cmd[0]);
 	return (0);
 }
 
@@ -71,7 +73,7 @@ int	main(int argc, char **argv, char **env)
 {
 	t_shell	shell;
 
-	val_return_process = 0;
+	status = 0;
 	if (argc != 1 || argv[1])
 		return (0);
 	start_struct(&shell, env);
@@ -81,5 +83,5 @@ int	main(int argc, char **argv, char **env)
 		check_command(&shell);
 		free_list_string(shell.split_cmd);
 	}
-	return (val_return_process);
+	return (status);
 }
