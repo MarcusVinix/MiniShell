94cf8c9cb8c53cb3d227ce78b2359bcf38b2c490
jhonatan229 <jhonatan229@outlook.com>
2021-10-06 21:16:05 -0300

    arrumando bug no export que incluia uma variavel de ambiente que começava com '.', numeros, '?' '-'

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: jestevam < jestevam@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 15:01:03 by jestevam          #+#    #+#             */
-/*   Updated: 2021/10/05 20:24:46 by jestevam         ###   ########.fr       */
+/*   Updated: 2021/10/06 21:14:05 by jestevam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,11 +43,17 @@ char **ft_split_v2(char *str, char c)
 	return (rslt);
 }
 
-static void	check_is_exist(t_list *lst, char *new_env)
+static int	check_is_exist(t_list *lst, char *new_env)
 {
 	char **key_val;
 
 	key_val = ft_split_v2(new_env, '=');
+	if (key_val[0][0] == '.' || ft_isdigit(key_val[0][0]) || key_val[0][0] == '?')
+	{
+		printf("bash: export: `%s': not a valid identifier\n", new_env);
+		free_list_string(key_val);
+		return (-1);
+	}
 	if (!change_value(&lst, key_val[0], key_val[1]))
 	{
 		if (key_val[1])
@@ -55,6 +61,7 @@ static void	check_is_exist(t_list *lst, char *new_env)
 					ft_strdup(key_val[1])));
 	}
 	free_list_string(key_val);
+	return (0);
 }
 
 void ft_export(t_shell *sh)
