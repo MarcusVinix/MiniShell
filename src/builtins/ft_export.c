21f7f0dd1cb99fa8e501bfaf01002ce4601a6c5c
jhonatan229 <jhonatan229@outlook.com>
2021-11-17 19:28:15 -0300

    arrumando arquitetura do arquivo ft_export.c

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_export.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mavinici <mavinici@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: jestevam < jestevam@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 15:01:03 by jestevam          #+#    #+#             */
-/*   Updated: 2021/11/16 20:20:32 by mavinici         ###   ########.fr       */
+/*   Updated: 2021/11/17 19:26:22 by jestevam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void printlst(char *key, char *value, int fd, int sig)
+static void	printlst(char *key, char *value, int fd, int sig)
 {
 	if (sig)
 	{
@@ -24,11 +24,11 @@ static void printlst(char *key, char *value, int fd, int sig)
 	}
 }
 
-char **ft_split_v2(char *str, char c)
+char	**ft_split_v2(char *str, char c)
 {
-	int count;
-	int len;
-	char **rslt;
+	int		count;
+	int		len;
+	char	**rslt;
 
 	count = 0;
 	len = ft_strlen(str);
@@ -44,22 +44,21 @@ char **ft_split_v2(char *str, char c)
 		if (count == len)
 			rslt[1] = ft_strdup(" ");
 		else
-			rslt[1] = ft_substr(str, count, len);	
+			rslt[1] = ft_substr(str, count, len);
 	}
-		
 	return (rslt);
 }
 
 static void	check_is_exist(t_list *lst, char *new_env, int sig, t_shell *sh)
 {
-	char **key_val;
+	char	**key_val;
 
 	key_val = ft_split_v2(new_env, '=');
 	if (!change_value(&lst, key_val[0], key_val[1], sig))
 	{
 		if (key_val[1])
 		{
-			ft_lstadd_back(&lst, ft_lstnew(ft_strdup(key_val[0]), 
+			ft_lstadd_back(&lst, ft_lstnew(ft_strdup(key_val[0]),
 					ft_strdup(key_val[1]), sig));
 			sh->len_env++;
 		}
@@ -67,54 +66,19 @@ static void	check_is_exist(t_list *lst, char *new_env, int sig, t_shell *sh)
 	free_list_string(key_val);
 }
 
-static void exporting_key(char *str, t_shell *sh)
-{
-	char **key_val;
-	char *value;
-	char *str_val;
-	char *new_val;
-
-	if (ft_strlen(str) == 1)
-		return (invalid_identifier(str));
-	str++;
-	key_val = ft_split_v2(str, '=');
-	value = find_value(&sh->lst_env, key_val[0]);
-	new_val = ft_strjoin("=", key_val[1]);
-	if (value)
-	{
-		str_val = ft_strjoin(value, new_val);
-		if (key_val[1])
-			invalid_identifier(str_val);
-		else
-			invalid_identifier(value);
-		free(str_val);
-	}
-	else
-		invalid_identifier(new_val);
-	free(new_val);
-	free_list_string(key_val);
-}
-
 //signal 1 = print error;
 //signal 2 = just return the number of error
-static int verify_valid(char *str, int signal, t_shell *sh)
+static int	verify_valid(char *str)
 {
 	if (str[0] == '-')
 	{
-		if (signal == 1)
-			invalid_option(str);
+		invalid_option(str);
 		return (2);
 	}
-	else if (str[0] == '$')
+	else if (str[0] == '.' || ft_isdigit(str[0])
+		|| str[0] == '?' || str[0] == '/')
 	{
-		if (signal == 1)
-			exporting_key(str, sh);
-		return (1);
-	}
-	else if (str[0] == '.' || ft_isdigit(str[0]) || str[0] == '?')
-	{
-		if (signal == 1)
-			invalid_identifier(str);
+		invalid_identifier(str);
 		return (1);
 	}
 	return (0);
@@ -122,7 +86,7 @@ static int verify_valid(char *str, int signal, t_shell *sh)
 
 int	ft_export(t_shell *sh, int fd, int sig)
 {
-	int count;
+	int	count;
 	int	resp;
 
 	resp = 0;
@@ -131,10 +95,10 @@ int	ft_export(t_shell *sh, int fd, int sig)
 		fd = 1;
 	while (sh->split_cmd[count])
 	{
-		if (verify_valid(sh->split_cmd[count], 1, sh))
+		if (verify_valid(sh->split_cmd[count]))
 		{
 			if (!resp)
-				resp = verify_valid(sh->split_cmd[count], 2, sh);
+				resp = verify_valid(sh->split_cmd[count]);
 		}
 		else
 			check_is_exist(sh->lst_env, sh->split_cmd[count], sig, sh);
